8adcfdafb584f4676bd7140cd0aebded1129ad52
Michael Schmitz <michaelschmitz5@gmail.com>
2017-04-08 20:58:37 -0600

    Revert "removed unnecessary import"
    This reverts commit 723fbce0e0cb19c92b6a98dc1b7f5530317c8be5.

--- src/roadgraph/MapGraph.java
+++ src/roadgraph/MapGraph.java
@@ -20,6 +20,7 @@ import java.util.function.Consumer;
 
 import geography.GeographicPoint;
 import util.GraphLoader;
+import week3example.MazeNode;
 
 /**
  * @author UCSD MOOC development team and YOU
